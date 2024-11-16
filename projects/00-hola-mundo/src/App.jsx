f8b78c01d4afdd8e8fe16e304103b1da1bd3ea72
Nando Javier Agüero <nandoaguerojs@gmail.com>
2024-11-16 11:36:06 -0300

    uso de props y cambio de estados

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,11 +1,37 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
+import { useState } from 'react'
 
 export function App () {
+    const formatUserName = (userName) => `@${userName}`;
+    /*
+    otra opción:
+    const formatedUserName = <span>@midudev</span>;
+    */
+    const [name, setName] = useState('midudev');
+    //cuando cambia un elemento padre, todos los hijos se vuelven a renderizar independientemente de si cambiaron o no
     return (
         <>
-            <TwitterFollowCard userName="midudev" name="Miguel Ángel Durán" />
-            <TwitterFollowCard userName="midudev" name="Miguel Ángel Durán" />
+            <section className="App">
+                <TwitterFollowCard formatUserName= {formatUserName} initialIsFollowing= { true } userName={name}>
+                    {/*lo que paso acá será el children, puede ser texto o elementos html, incluso otro componente, el componente recibe el children como parámetro*/}
+                    Miguel Ángel Durán 1
+                </TwitterFollowCard>
+                <TwitterFollowCard formatUserName= {formatUserName} userName={name}>
+                    Miguel Ángel Durán 2
+                </TwitterFollowCard>
+                {/*tambien puedo pasar un objeto, pero puede ser una mala práctica*
+                const user = {
+                    userName: 'midudev',
+                    isFollowing: true,
+                    name: 'Miguel Ángel Durán'
+                }
+                <TwitterFollowCard {... user}/>
+                */}
+                <button onClick={()=> setName('pedromichel')}>
+                    Cambiar nombre
+                </button>
+            </section>
         </>
     )
 }
--- projects/00-hola-mundo/src/TwitterFollowCard.jsx
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,16 +1,37 @@
-export function TwitterFollowCard ({ userName, name, isFollowing}) {
+import { useState } from 'react'
+//podría darle un valor por defecto a las props por si no se pasa el valor, como userName = 'unknown'
+export function TwitterFollowCard ({children, formatUserName,initialIsFollowing, userName}) {
+    /*const state = useState(false);
+    const isFollowing = state[0]; //valor del estado
+    const setIsFollowing = state[1]; //modificar el estado*/
+    //no podría hacer el cambio de estado en un nivel superior ya que el estado se inicializa una vez
+    const [isFollowing, setIsFollowing] = useState(initialIsFollowing); //le pasamos a useState el valor inicial
+    const handleClick = () => {
+        setIsFollowing(!isFollowing);
+    }
+    /*
+        nunca modificar props:
+            userName = `@{userName}`
+        en su lugar, podria crear otra variable:
+            const userNameModified = `@{userName}`
+    */
+    const text = isFollowing ? 'Siguiendo' : 'Seguir';
+    const buttonClassName = isFollowing
+        ? 'tw-followCard-button isFollowing'
+        : 'tw-followCard-button'
+    console.log(isFollowing)
     return (
         <article className='tw-followCard'>
             <header className='tw-followCard-header'>
                 <img className='tw-followCard-avatar' src={`https://unavatar.io/${userName}`} alt="El avatar de midudev" />
                 <div className='tw-followCard-info'>
-                    <strong>Miguel Ángel Durán</strong>
-                    <span className='tw-followCard-infoUserName'>@midudev</span>
+                    <strong>{children}</strong>
+                    <span className='tw-followCard-infoUserName'>{formatUserName(userName)}</span>
                 </div>
             </header>
             <aside>
-                <button className='tw-followCard-button'>
-                    Seguir
+                <button className={buttonClassName} onClick={handleClick}>
+                    {text}
                 </button>
             </aside>
         </article>
